a6ec1e65480cf11ff502340596770aaf5d66a9b1
DESKTOP-C34H2AK\jorge <machado.jorge23@gmail.com>
2019-06-04 23:55:20 +0100

    add project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var blah = new Class1();
+            Assert.AreEqual(4,blah.Blah());
+        }
+    }
+}
